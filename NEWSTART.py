aa7ab395aa654b2f6e9707aa2b685148ee4873ae
JustAreapeR <119354649+JustAreapeR@users.noreply.github.com>
2023-03-15 22:31:22 +0530

    Create NEWSTART.py
    Starting from scratch as the old code had too many errors.
    i have a perfect path build for the new code.

---
+++ NEWSTART.py
@@ -0,0 +1,17 @@
+#starting from scratch(for more info open readme text file)
+
+def printBoard(xState, zState):
+    zero = 'X' if xState[0] else ('O' if zState[0] else 0)
+    one = 'X' if xState[1] else ('O' if zState[1] else 1)
+    two = 'X' if xState[2] else ('O' if zState[2] else 2)
+    three = 'X' if xState[3] else ('O' if zState[3] else 3)
+    four = 'X' if xState[4] else ('O' if zState[4] else 4)
+    five = 'X' if xState[5] else ('O' if zState[5] else 5)
+    six = 'X' if xState[6] else ('O' if zState[6] else 6)
+    seven = 'X' if xState[7] else ('O' if zState[7] else 7)
+    eight = 'X' if xState[8] else ('O' if zState[8] else 8)
+    print("{0} | {1} | {2} ")
+    print("----|-----|-----")
+    print("{3} | {4} | {5} ")
+    print("----|-----|-----")
+    print("{6} | {7} | {8} ")  #Created this new board(hope on discord for further discussion)
