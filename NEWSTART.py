9c29cc4804a2a6b2667b5a6527e815c3d9a1cf86
JustAreapeR <119354649+JustAreapeR@users.noreply.github.com>
2023-03-15 22:51:54 +0530

    Update NEWSTART.py

--- NEWSTART.py
+++ NEWSTART.py
@@ -19,13 +19,13 @@ def print_Board(Player_1, Player_2):
 def sum(x, y, z):
     return x + y + z
 
- def checkWin(xState, zState):
+def check_for_Win(Player_1, Player_2):
     wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
     for win in wins:
-        if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
-            print("X Won the match")
-            return 1
-        if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3):
-            print("O Won the match")
+        if(sum(Player_1[win[0]], Player_1[win[1]], Player_1[win[2]]) == 3):
+            print("P1 WINS")
+            return 1 #1 for p1 and 0 for p2
+        if(sum(Player_2[win[0]], Player_2[win[1]], Player_2[win[2]]) == 3):
+            print("P2 WINS")
             return 0
-    return -1
+    return - 1
